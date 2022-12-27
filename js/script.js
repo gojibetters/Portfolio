f2bbe71800e19ee9e2b83cfba20ceaa94c9bbb4b
GojiBetters <patezbr@gmail.com>
2022-12-27 11:30:23 -0300

    Get data from repo API

--- js/script.js
+++ js/script.js
@@ -8,10 +8,26 @@ fetch('https://api.github.com/users/gojibetters')
     const elementName = document.querySelector('.name')
     const elementBio = document.querySelector('.bio')
 
-    //repo
-    
-
     elementPhoto.innerHTML = `<img src="${data.avatar_url}"/>`
     elementName.innerHTML = data.name
     elementBio.innerHTML = data.bio
+  })
+
+fetch('https://api.github.com/users/gojibetters/repos')
+  .then(response => response.json())
+  .then(response => {
+    const data = response
+
+    //repo
+    const elementTitle = document.querySelector('.title')
+    const elementDescription = document.querySelector('.description')
+    const elementStars = document.querySelector('.stars')
+    const elementForks = document.querySelector('.forks')
+    const elementLanguage = document.querySelector('.language')
+
+    elementTitle.innerHTML = `<a href="${data[13].html_url}" target="_blank">${data[13].name} </a>`
+    elementDescription.innerHTML = data[13].description
+    elementStars.innerHTML = data[13].stargazers_count
+    elementForks.innerHTML = data[13].forks
+    elementLanguage.innerHTML = data[13].language
   })
