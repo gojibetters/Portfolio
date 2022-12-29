c7f4ec9fd75735d8afae1141d502e47fa4ab5ad8
GojiBetters <patezbr@gmail.com>
2022-12-29 13:16:15 -0300

    New icons

--- js/script.js
+++ js/script.js
@@ -19,11 +19,11 @@ fetch('https://api.github.com/users/gojibetters')
     const elementLinkedin = document.querySelector('.linkedin')
     const elementEmail = document.querySelector('.email')
 
-    elementLocation.innerHTML = `<img src="assets/map-pin.png"/><a href="https://www.google.com/search?q=brazil" target="_blank">Brazil</a>`
-    elementCompany.innerHTML = `<img src="assets/briefcase.png" /><a href="https://lab2dev.com/" target="_blank">Lab2Dev</a>`
-    elementGithub.innerHTML = `<img src="assets/github.png" /><a href="https://github.com/gojibetters" target="_blank">Gojibetters</a>`
-    elementLinkedin.innerHTML = `<img src="assets/linkedin.png" /><a href="https://www.linkedin.com/in/brunopatez/" target="_blank">Bruno Patez</a>`
-    elementEmail.innerHTML = `<img src="assets/mail.png" /><a href="mailto:brunoapatez@gmail.com">Send email</a>`
+    elementLocation.innerHTML = `<img src="assets/map-pin.svg"/><a href="https://www.google.com/search?q=brazil" target="_blank">Brazil</a>`
+    elementCompany.innerHTML = `<img src="assets/briefcase.svg" /><a href="https://lab2dev.com/" target="_blank">Lab2Dev</a>`
+    elementGithub.innerHTML = `<img src="assets/github.svg" /><a href="https://github.com/gojibetters" target="_blank">Gojibetters</a>`
+    elementLinkedin.innerHTML = `<img src="assets/linkedin.svg" /><a href="https://www.linkedin.com/in/brunopatez/" target="_blank">Bruno Patez</a>`
+    elementEmail.innerHTML = `<img src="assets/mail.svg" /><a href="mailto:brunoapatez@gmail.com">Send email</a>`
   })
 
 fetch('https://api.github.com/users/gojibetters/repos')
@@ -37,15 +37,15 @@ fetch('https://api.github.com/users/gojibetters/repos')
     <div class="description"></div>
     <div class="stats">
       <div class="starIcon">
-        <img src="assets/star.png" />
+        <img src="assets/star.svg" />
         <div class="stars"></div>
       </div>
       <div class="forksIcon">
-        <img src="assets/git-branch.png" />
+        <img src="assets/git-branch.svg" />
         <div class="forks"></div>
       </div>
       <div class="languageIcon">
-        <img src="assets/yellipse.png" />
+        <img src="assets/yellipse.svg" />
         <div class="language"></div>
       </div>
     </div>
@@ -63,6 +63,11 @@ fetch('https://api.github.com/users/gojibetters/repos')
     const elementLanguage = document.getElementsByClassName('language')
 
     for (let i = 0; i < elementTitle.length; i++) {
+      if (data[i].description == null) {
+        data[i].description = 'empty description'
+      } else if (data[i].language == null) {
+        data[i].language = 'unknown'
+      }
       elementTitle[
         i
       ].innerHTML = `<a href="${data[i].html_url}" target="_blank">${data[i].name}</a>`
